45b336bb35d298fcb5afc5f86d9d5ee63b0f7c00
vincenzodivita <divitavincenzo@icloud.com>
2023-06-06 22:37:31 +0200

    Add css link

--- index.php
+++ index.php
@@ -19,6 +19,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Bad Words</title>
+        <link rel="stylesheet" href="style.css">
     </head>
     <body>
         <h1>Bad Words</h1>
