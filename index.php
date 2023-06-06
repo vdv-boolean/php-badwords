692b60d53c855076fa8fc1605e937b30108e6a1b
vincenzodivita <divitavincenzo@icloud.com>
2023-06-06 22:36:19 +0200

    Add php code

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<?php
+    # Text string variables
+    $text_string = 'Il testo è:' ;
+    $bad_string = 'La parola censurata è:';
+    $censored_string = 'Il testo censurato è:';
+
+    # User text variables
+    $long_text = $_GET['long-text']; 
+    $bad_text = $_GET['bad-text'];
+
+    # Censored text function
+    $censored_text = str_replace($bad_text, '***', $long_text) ;
+?>
+
 <!DOCTYPE html>
 <html lang="it">
     <head>
@@ -7,6 +21,13 @@
         <title>Bad Words</title>
     </head>
     <body>
-        <h1>Questo è l'index.php</h1>
+        <h1>Bad Words</h1>
+        <h2><?= $text_string; ?></h2>
+        <p><?= $long_text; ?></p>
+        <h2><?= $bad_string; ?></h2>
+        <span><?= $bad_text; ?></span>
+        <h2><?= $censored_string; ?></h2>
+        <p><?= $censored_text; ?></p>
+
     </body>
 </html>
